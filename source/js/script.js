1ed295d5974d227b5e83265d093f7c7abe636113
SergeiGray <borodat_for@mail.ru>
2019-05-02 00:33:23 +0300

    Правки

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,18 @@
 let menuContainer = document.querySelector('.menu__container');
 let menuToggle = document.querySelector('.menu__menu-toggle');
+let menuToggleCross = document.querySelector('.menu__menu-toggle--cross');
+let menuToggleBurger = document.querySelector('.menu__menu-toggle--burger');
 let menu = document.querySelector('.menu');
 let menuHead = document.querySelector('.menu__head');
 menuContainer.classList.add('menu__script-on');
-menuToggle.classList.add('menu__script-on--menu-toggle');
+menuToggleCross.classList.add('menu__script-on--menu-toggle-off');
+menuToggleBurger.classList.remove('menu__script-on--menu-toggle-off');
 menu.classList.add('menu__script-on--menu');
 menuHead.classList.add('menu__script-on--menu-head');
 menuToggle.addEventListener('click', function() {
   menuContainer.classList.toggle('menu__script-on');
-  menuToggle.classList.toggle('menu__script-on--menu-toggle');
+  menuToggleCross.classList.toggle('menu__script-on--menu-toggle-off');
+  menuToggleBurger.classList.toggle('menu__script-on--menu-toggle-off');
   menu.classList.toggle('menu__script-on--menu');
   menuHead.classList.toggle('menu__script-on--menu-head');
 })
